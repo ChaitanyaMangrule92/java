6bd506d535c44682ca949be536d5bc55846730c3
ChaitanyaMangrule92 <105437515+ChaitanyaMangrule92@users.noreply.github.com>
2022-05-12 19:14:34 +0530

    Add files via upload

---
+++ Practical/charDataType.java
@@ -0,0 +1,17 @@
+package Chaitanya;
+
+public class charDataType {
+
+	public static void main(String[] args) {
+		char marks, grade; // declare char datatype variable
+		// initialize value to variable
+		marks = '8';
+		grade = 'B';
+		// printing on console
+		System.out.println("Marks:"+marks);
+		System.out.println("Grade: "+grade);
+
+
+	}
+
+}
