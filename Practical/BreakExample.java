9618a0660e3743d90d34af9843d3ff64c452a432
ChaitanyaMangrule92 <105437515+ChaitanyaMangrule92@users.noreply.github.com>
2022-05-20 18:49:43 +0530

    Add files via upload

---
+++ Practical/BreakExample.java
@@ -0,0 +1,25 @@
+package CM5;
+
+public class BreakExample {
+
+
+public static void main(String[] args) { 
+
+
+
+for(int i=2;i<=8;i++){ if(i==6){
+break;
+
+}
+
+System.out.println(i);
+
+
+}
+
+}
+
+}
+
+
+
---
+++ Practical/DoWhileLoopExample.java
@@ -0,0 +1,16 @@
+package CM5;
+
+public class DoWhileLoopExample {
+
+	public static void main(String args[]){ int i=7;
+	do{
+
+	System.out.println(i); i--;
+	}while(i>3);
+
+	}
+
+	}
+	 
+
+
