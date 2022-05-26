be6c92aa96c67fccc079f5061bafe3fa983c8669
ChaitanyaMangrule92 <105437515+ChaitanyaMangrule92@users.noreply.github.com>
2022-05-26 18:55:10 +0530

    Add files via upload

---
+++ Lab Activity/EvenNumbers.java
@@ -0,0 +1,18 @@
+package Chaitanya;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//number
+		int n = 20;
+		
+		//print all even numbers <=n 
+		for (int i=2; i<=n; i+=2) {
+			System.out.print(i+"  ");
+		}
+	}
+	
+
+}
