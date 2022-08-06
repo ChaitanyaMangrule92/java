3cd932d4271aec2d0bc1686ff6ab15a99a5bb810
ChaitanyaMangrule92 <105437515+ChaitanyaMangrule92@users.noreply.github.com>
2022-08-06 13:18:21 +0530

    Add files via upload

---
+++ Lab Activity/PatternEg.java
@@ -0,0 +1,17 @@
+package Lab;
+
+public class PatternEg {
+
+	 public static void main(String arg[]){
+		   for(int i=1;i<=4;i++){
+		    for(int j=3;j>=i;j--){
+		        System.out.print("  ");  
+		    }
+		    for(int k=1;k<=i;k++){
+		        System.out.print(" java");
+		    }
+		    System.out.println();
+		   }       
+		    
+		}
+		}
---
+++ Lab Activity/TwoDimensionalArray.java
@@ -0,0 +1,21 @@
+package Lab;
+
+public class TwoDimensionalArray {
+
+	 public static void main(String []arg){
+		 
+	        int a[][]={{10,20,30},{40,50,60}};
+	        
+	        for(int i=0;i<a[0].length;i++){
+	        	
+	            for(int j=0;j<a.length;j++){
+	            	
+	            System.out.print(a[j][i]+"\t");
+	           
+	        }
+	        System.out.println( );
+	    }
+
+	}
+	
+}
